a55f80ead4dc43b8a140f4cac367791f2661cd96
Maximiliano <oscaralfonso93@hotmail.com>
2021-05-29 22:41:05 -0300

    Update RF_Jobs.user.js

--- RF_Jobs.user.js
+++ RF_Jobs.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         RF_Jobs
 // @namespace    none
-// @version      1.0
+// @version      2.0
 // @description  Data about RainfresotQa jobs
 // @author       ixamy
 // @match        https://portal.rainforestqa.com/jobs*
@@ -13,23 +13,33 @@
 (function() {
     'use strict';
 	var paid = 0,
+		jobCount = 0,
 		tableBody = document.getElementsByTagName('tbody')[0],
-		date = new Date(),
-		currentDateUTC = date.getUTCDate(),
-		rfDateUTC = new Date(tableBody.rows[0].cells[4].innerText).getDate();
+		tableLength = tableLength= tableBody.rows.length,
 
-	//print every paid amount
-	for(var i=0;i<25;i++){
-        rfDateUTC = new Date(tableBody.rows[i].cells[4].innerText).getDate();
-		if(rfDateUTC == currentDateUTC){
+		date = new Date(),
+		currentDate = [date.getUTCFullYear(),
+					   date.getUTCMonth()+1,
+ 					   date.getUTCDate()],
+
+		hourUTC = date.getUTCHours() + ':' + date.getUTCMinutes();
+
+	for(var i=0;i<tableLength;i++){
+		var rfFullDate = new Date(tableBody.rows[i].cells[4].innerText),
+			rainforestDate = [rfFullDate.getUTCFullYear(),
+							  rfFullDate.getUTCMonth()+1,
+							  rfFullDate.getUTCDate()]
+		if(currentDate.toString() == rainforestDate.toString()){
 			paid += parseInt(tableBody.rows[i].cells[8].innerText.split('c')[0]);
+			console.log(rainforestDate);
+			jobCount+=1;
 		}
     }
 
     GM_notification({
-        title: "Date: " + currentDateUTC,
-        text: "Paid: " + paid/100,
+        title: "Date: " + currentDate.join('/'),
+        text: "Paid: $" + paid/100 + "\nJob Count: " + jobCount + "\nUTC Time: " + hourUTC,
         image: "http://image.prntscr.com/image/2fc8f4257d86482681223bdcfb4170fd.png",
-        timeout: 5000
+        timeout: 6000
     });
 })();
