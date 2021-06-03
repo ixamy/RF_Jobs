1bd4f88b5f388a6d19529de9cf17f5ecc20b3aef
Maximiliano <oscaralfonso93@hotmail.com>
2021-06-03 00:16:25 -0300

    Update RF_Jobs.user.js

--- RF_Jobs.user.js
+++ RF_Jobs.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         RF_Jobs
 // @namespace    none
-// @version      2.0
+// @version      2.1
 // @description  Data about RainfresotQa jobs
 // @author       ixamy
 // @match        https://portal.rainforestqa.com/jobs*
@@ -14,6 +14,8 @@
     'use strict';
 	var paid = 0,
 		jobCount = 0,
+		cellEight,
+		cellSeven,
 		tableBody = document.getElementsByTagName('tbody')[0],
 		tableLength = tableLength= tableBody.rows.length,
 
@@ -26,19 +28,35 @@
 
 	for(var i=0;i<tableLength;i++){
 		var rfFullDate = new Date(tableBody.rows[i].cells[4].innerText),
-			rainforestDate = [rfFullDate.getUTCFullYear(),
-							  rfFullDate.getUTCMonth()+1,
-							  rfFullDate.getUTCDate()]
+			rainforestDate = [rfFullDate.getFullYear(),
+							  rfFullDate.getMonth()+1,
+							  rfFullDate.getDate()]
 		if(currentDate.toString() == rainforestDate.toString()){
-			paid += parseInt(tableBody.rows[i].cells[8].innerText.split('c')[0]);
-			console.log(rainforestDate);
-			jobCount+=1;
+			cellEight = tableBody.rows[i].cells[8].innerText;
+			cellSeven = tableBody.rows[i].cells[7].innerText
+
+			if(cellEight!="—"){
+				paid += parseInt(tableBody.rows[i].cells[8].innerText.split('c')[0]);
+			} else {
+				paid += parseInt(tableBody.rows[i].cells[7].innerText.split('c')[0]);
+			} jobCount+=1;
+
+			if(cellEight=="—" && cellSeven=="—"){
+			   jobCount-=1;}
 		}
     }
 
+	function checkJobs(){
+		if(jobCount==0){
+			return "No jobs for the range loaded.";
+		} else {
+			return "Paid: $" + paid/100 + "\nJob Count: " + jobCount + "\nUTC Time: " + hourUTC;
+		}
+	}
+
     GM_notification({
         title: "Date: " + currentDate.join('/'),
-        text: "Paid: $" + paid/100 + "\nJob Count: " + jobCount + "\nUTC Time: " + hourUTC,
+        text: checkJobs(),
         image: "http://image.prntscr.com/image/2fc8f4257d86482681223bdcfb4170fd.png",
         timeout: 6000
     });
